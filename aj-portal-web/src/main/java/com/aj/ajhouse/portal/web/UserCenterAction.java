369c34074d1987888af67d31e91134ffa315d02a
joneyan <joneyan@example.com>
2017-11-25 11:48:09 +0800

    严静:后台全部完成,项目整合完毕2

--- aj-portal-web/src/main/java/com/aj/ajhouse/portal/web/UserCenterAction.java
+++ aj-portal-web/src/main/java/com/aj/ajhouse/portal/web/UserCenterAction.java
@@ -1,18 +1,10 @@
 package com.aj.ajhouse.portal.web;
 
-import com.aj.ajhouse.common.dto.Order;
-import com.aj.ajhouse.common.dto.Page;
-import com.aj.ajhouse.pojo.po.AjSysnews;
-import com.aj.ajhouse.service.UserCenterService;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,8 +22,8 @@ import java.util.Map;
 @Scope("prototype")
 public class UserCenterAction {
 
-    @Autowired
-    UserCenterService userCenterService;
+    /*@Autowired
+    private UserCenterService userCenterService;*/
     /**
      *用户中心跳转
      * @return
@@ -43,58 +35,59 @@ public class UserCenterAction {
 
     /**
      * 系统消息Action
-     *@param tel 用户手机
      * @return
      */
-    @RequestMapping("/messages/{tel}")
-    public String getMessages(@PathVariable(value="tel") String tel ,Page page, Order order){
+   /* @RequestMapping("/messages/{id}")
+    public String getMessages(Page page, Order order){
         System.out.println("messages");
         List<AjSysnews> list =null;
         list =userCenterService.getMessages( page,  order);
-        System.out.println(tel);
-        return "userCenter/userCenterMess";
-    }
+        System.out.println(list);
+        return "m";
+    }*/
 
     /**
      *
      * 我的提问Action
-     * @param tel 用户手机
+     * @param id 用户id
      * @param map
      * @return
      */
-    @RequestMapping("/questMessages/{tel}")
-    public String questMessages( @PathVariable(value="tel") String tel , Map<String,Object> map){
+    @RequestMapping("/questMessages/{id}")
+    public String questMessages( @PathVariable(value="id") Integer id , Map<String,Object> map){
         System.out.println("questMessages");
-       System.out.println(tel);
-        return "userCenter/userCenterQuestMess";
+        int i =id;
+        System.out.println(i);
+        return "m";
     }
 
     /**
      *
      * 我的回答Action
-     * @param tel 用户手机
+     * @param id 用户id
      * @param map
      * @return
      */
-    @RequestMapping("/questionAns/{tel}")
-    public String questionAns( @PathVariable(value="tel") String tel ,Map<String,Object> map){
+    @RequestMapping("/questionAns/{id}")
+    public String questionAns( @PathVariable(value="id") Integer id , Map<String,Object> map){
         System.out.println("questionAns");
-        System.out.println(tel);
-        return "userCenter/userCenterQuestAns";
+        int i =id;
+        System.out.println(i);
+        return "m";
     }
 
     /**
      * 修改资料
-     * @param tel 用户手机
+     * @param id  用户id
      * @param map
      * @return
      */
-     @RequestMapping("/userX/{tel}")
-    public String userX( @PathVariable(value="tel") String tel , Map<String,Object> map){
+     @RequestMapping("/userX/{id}")
+    public String userX( @PathVariable(value="id") Integer id , Map<String,Object> map){
         System.out.println("userX");
-
-      System.out.println(tel);
-        return "userCenter/userCenterUserX";
+        int i =id;
+        System.out.println(i);
+        return "m";
     }
 
 
